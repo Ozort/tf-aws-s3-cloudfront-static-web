9f27bafeb8e4990d49de47c358d7a80ed39de17d
Ozort <abubobby04@gmail.com>
2026-09-23 15:39:47 -0400

    initial commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+    description = "My s3 bucket"
+  default = "zort-bucket-505"
+}
+
+variable "website_index_document" {
+  description = "This is the website index document"
+  default = "index.html"
+}
